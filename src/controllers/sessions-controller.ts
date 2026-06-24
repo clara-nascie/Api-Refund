000a150b76cd6bb3d659643bd9c3f39b9d96a5c8
Your Name <you@example.com>
2026-06-24 18:45:02 -0300

    feat: implement SessionController with basic email and password validation schema

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -4,8 +4,23 @@ import { z } from "zod";
 // Cria a sessão do usuario
 class SessionController {
   async create(req: Request, res: Response) {
-    return res.json({ message: "Login realizado com sucesso" });
-  }
+    const bodySchema = z.object({
+      email: z.string().email({message: "E-mail inválido"}),
+      password: z.string(),
+    });
+
+    const { email, password } = bodySchema.parse(req.body);
+
+    // aqui entra o prisma pra buscar na base de dados
+    // Se o usuario nao existir, retorna erro
+    // Se o usuario existir, retorna o usuario
+    // Se a senha estiver errada, retorna erro
+    
+
+
+    // Se tudo estiver certo, retorna o usuario
+     res.json({email, password});
+    }
 }
 
 export { SessionController };
